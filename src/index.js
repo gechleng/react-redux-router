1d1ce5126293509de31bbaef87522394b25ab455
ngorgechleng <ngorgechleng@gmail.com>
2018-05-17 10:05:03 +0700

    react with redux

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+//redux
+import { Provider} from 'react-redux';
+import store from './store';
+
+const app = (
+    <Provider store={store}>
+      <App />
+    </Provider>
+)
+
+ReactDOM.render(
+  app,
+  document.getElementById('root'));
+registerServiceWorker();
